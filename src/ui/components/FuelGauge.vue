27b16d5b922e81d59068b3f531a47af61128ec7f
vicroseinfinite <victordrose@gmail.com>
2026-08-26 10:00:01 -0500

    Move fuel gauge next to speedometer, restore emoji+bar style

--- src/ui/components/FuelGauge.vue
+++ src/ui/components/FuelGauge.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="fuel-gauge" :class="{ low: fraction < 0.25, empty: fraction <= 0 }">
-    <div class="fuel-bar-track">
-      <div class="fuel-bar-fill" :style="{ height: (fraction * 100) + '%' }"></div>
+    <span class="fuel-emoji">⛽</span>
+    <div class="fuel-bar">
+      <div class="fuel-fill" :style="{ width: (fraction * 100) + '%' }"></div>
     </div>
-    <div class="fuel-label">
-      <span class="fuel-pct">{{ Math.round(fraction * 100) }}</span>
-      <span class="fuel-unit">FUEL</span>
-    </div>
+    <span class="fuel-pct">{{ Math.round(fraction * 100) }}%</span>
   </div>
 </template>
 
